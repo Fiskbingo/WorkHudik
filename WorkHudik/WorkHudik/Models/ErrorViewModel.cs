8261e7b7208579f07814d5d71c2dfbf8e7349b77
Fiskbingo <52127810+Fiskbingo@users.noreply.github.com>
2019-12-18 12:08:43 +0100

    Add files via upload

---
+++ WorkHudik/WorkHudik/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WorkHudik.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
